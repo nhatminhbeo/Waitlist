06314c20b568282887a7e15671477d10c3d4ea92
Justin Huynh <juh079@ucsd.edu>
2017-01-14 12:38:09 -0800

    added list functionality, need to do edit page now

--- public/index.js
+++ public/index.js
@@ -1,3 +1,18 @@
+// =======================================================================
+// RESTful Routes
+// HTTP Verbs: GET, POST, PUT, DELETE
+//
+// Name     	|   Path      		|   HTTP Verb |   Purpose
+// =======================================================================
+// Index    	|   /:id      		|   GET       | Show the waitlist
+// Sign in  	|   /signin/   		|   GET       | Show form to signin
+// Edit     	|   /edit/:id 		|   GET       | Show form to edit a specific user
+// Get list 	|   /waitlist/ 		|   GET       | Get the userlist as json
+// Update list 	|   /waitlist/ 		|   POST      | Create new user
+// Delete list 	|   /waitlist/:id 	|   DELETE	  | Delete an user with id from waitlist
+// Get user 	|	/user/:id 		| 	GET 	  | Get an user with id as json
+// Edit user 	| 	/user/:id 		| 	PUT 	  | Edit an user with id as json
+// =======================================================================
 $(document).ready(function() {
 	// When the html page has been loaded, start here:
 	// alert("Hello");
