d6270c098108b5438b940f72948647c7db9d277d
Justin Huynh <juh079@ucsd.edu>
2017-01-14 11:35:11 -0800

    made changes to front of the ender

--- public/index.js
+++ public/index.js
@@ -2,25 +2,23 @@ $(document).ready(function() {
 	// When the html page has been loaded, start here:
 	// alert("Hello");
 
-	// Make a GET request to our backend
-	getPosts();
-
 	$("form").submit(function(event) {
 		// Will prevent default HTML form interaction
 		event.preventDefault();
 
 		// Gather data
-		var titleText = $("#title").val();
-		var postText = $("#post").val();
+		var name = $("#name").val();
+		var numberOfPeople = $("#nop").val();
+		var phoneNumber = $("#phoneNumber").val();
 
 		var data = {
-			title: titleText,
-			post: postText
+			name: name,
+			numberOfPeople: numberOfPeople,
+			phoneNumber: phoneNumber
 		};
 
 		//Fire off that data
-		$.post("/posts/", data, function() {
-			getPosts();
+		$.post("/waitlist/", data, function() {
 			console.log("posted!");
 		});
 	});
@@ -28,26 +26,3 @@ $(document).ready(function() {
 });
 
 
-function getPosts() {
-
-	// Reset content inside #content
-	$("#content").text("");
-
-	$.get('/posts/', function(data) {
-	// console.log(data);
-		var rows = data["rows"];
-		for (var i=0; i<rows.length; i++) {
-			// Find the important stuff inside data
-			var title = rows[i]["doc"]["title"];
-			var post = rows[i]["doc"]["post"];
-
-			// Generate the html we want to insert
-			var card='<div class="card"><h3 class="card--header">' +
-				title + '</h3>' + 
-				'<p class="card--content">' + post + '</p></div>';
-
-			// Insert the card HTML
-			$('#content').append(card);
-		}
-	});
-}
