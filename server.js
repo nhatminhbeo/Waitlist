f73e961cb3598dd239229b181a5b34427d5e8c78
nhatminhbeo <tqnmb437@gmail.com>
2017-01-14 14:32:35 -0800

    WOW

--- server.js
+++ server.js
@@ -7,12 +7,31 @@ var logger = require('morgan');
 
 // Create the database
 var PouchDB = require('pouchdb');
-var db = new PouchDB('database');
-db.put({
-	_id: 'waitlist',
-	list: []
-})
+var dbwaitlist = new PouchDB('waitlist');
+var dbuser = new PouchDB('user');
 
+var tmplist = {
+listid: 0,
+userlist: []
+};
+
+// initialize first restaurant waitlist (for prototype)
+dbwaitlist.allDocs({
+		include_docs: true,
+		attachments: true
+
+	}, function(err, result) {
+		if (err) {
+			res.sendStatus(400);
+		} else if (result.rows.length === 0) {
+			console.log("Successfully initialized dbwaitlist");
+			dbwaitlist.post(tmplist, function(err, created) {
+				if (err) {
+					console.log("Failed to initialize dbwaitlist");
+				} 
+			});		
+		}
+});
 
 
 var app = express();
@@ -44,7 +63,7 @@ app.enable('trust proxy');
 //
 // Name     	|   Path      		|   HTTP Verb |   Purpose
 // =======================================================================
-// Index    	|   /:id      		|   GET       | Show the waitlist
+// Index    	|   /app/:id      	|   GET       | Show the waitlist
 // Sign in  	|   /signin/   		|   GET       | Show form to signin
 // Edit     	|   /edit/:id 		|   GET       | Show form to edit a specific user
 // Get list 	|   /waitlist/ 		|   GET       | Get the userlist as json
@@ -54,79 +73,166 @@ app.enable('trust proxy');
 // Edit user 	| 	/user/:id 		| 	PUT 	  | Edit an user with id as json
 // =======================================================================
 
+// ====================
+//   front end
+// ====================
+app.get('/signin/', function(req, res) {
+	res.sendFile(__dirname + '/public/signin.html');
+});
+
+app.get('/app/:id', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html', {'id': id});
+});
+
+app.get('/edit/:id', function(req, res) {
+	res.sendFile(__dirname + '/public/edit.html', {'id': id});
+});
+
 
 // ====================
 //   waitlist resource
 // ====================
 app.get('/waitlist/', function(req, res) {
-	db.allDocs({
-		include_docs: true;
-		attachments: true
-	}, function(err, result)) {
-		if (err) {
-			res.sendStatus(400);
-		} else {
-			var data = [];
 
-			for i in 
-		}
-	}
-})
 
-// get(routes, callbacks)
-app.get('/posts/', function(req, res) {
-
-	db.allDocs({
+	// build flat list based on waitlist, then response the list
+	dbwaitlist.allDocs({
 		include_docs: true,
 		attachments: true
-
 	}, function(err, result) {
+		var data = [];
+		var j = 0
 		if (err) {
 			res.sendStatus(400);
 		} else {
-			res.status(200).json(result);
+			
+			//res.status(200).json(result);
+			//return;
+			for (var i=0; i<result.rows[0].doc.userlist.length; i++) {
+				dbuser.get(result.rows[0].doc.userlist[i], function(err, found) {
+					// console.log(found);
+					if (err) {
+						res.sendStatus(400);
+					} else {
+						data.push(found);
+						j++;
+						if (j === result.rows[0].doc.userlist.length) {
+							console.log(i);
+							res.status(200).json(data);
+							return;
+						}
+					//data2 = JSON.stringify(data);
+				}
+				
+				});
+			}
+			
 		}
 	});
 });
 
-app.post('/posts/', function(req, res) {
+app.post('/waitlist/', function(req, res) {
+
+	// get data from request
 	var post = {
-		title: req.body.title,
-		post: req.body.post
+		name: req.body.name,
+		phone: req.body.phone,
+		guests: req.body.guests,
+		preferences: req.body.preferences
 	};
 
-	db.post(post, function(err, created) {
+
+	// create new user
+	dbuser.post(post, function(err, created) {
 		if (err) {
+			console.log("failed to create user");
 			res.sendStatus(400);
 		} else {
-			res.status(201).json(created);
+				// enqueue
+				dbwaitlist.allDocs({
+					include_docs: true,
+					attachments: true
+				}, function(err, result) {
+					if (err) {
+						res.sendStatus(400);
+					} else {
+						var doc = result.rows[0].doc;
+						doc.userlist.push(created.id);
+						dbwaitlist.put(doc, function(err, response) {
+							if (err) {
+								res.sendStatus(400);
+							}
+							else {
+								res.status(200).json(created);
+							}
+						});
+						
+					}
+				});
 		}
-
-	});
-});
-
-app.delete('/posts/:id', function(req, res) {
-
-	db.get(req.params.id, function(err, found) {
-		if (err) {
-			res.sendStatus(400); 
-		} else {
-			db.remove(found, function(err, removed) {
-				if (err) {
-					res.sendStatus(400);
-				} else {
-					res.sendStatus(202);
-				}
-			});
-		}
 	});
 
 
+
 });
 
-app.get('/app', function(req, res) {
-	res.sendFile(__dirname + '/public/index.html');
-});
+
+
+
+// // get(routes, callbacks)
+// app.get('/posts/', function(req, res) {
+
+// 	db.allDocs({
+// 		include_docs: true,
+// 		attachments: true
+
+// 	}, function(err, result) {
+// 		if (err) {
+// 			res.sendStatus(400);
+// 		} else {
+// 			res.status(200).json(result);
+// 		}
+// 	});
+// });
+
+// app.post('/posts/', function(req, res) {
+// 	var post = {
+// 		title: req.body.title,
+// 		post: req.body.post
+// 	};
+
+// 	db.post(post, function(err, created) {
+// 		if (err) {
+// 			res.sendStatus(400);
+// 		} else {
+// 			res.status(201).json(created);
+// 		}
+
+// 	});
+// });
+
+// app.delete('/posts/:id', function(req, res) {
+
+// 	db.get(req.params.id, function(err, found) {
+// 		if (err) {
+// 			res.sendStatus(400); 
+// 		} else {
+// 			db.remove(found, function(err, removed) {
+// 				if (err) {
+// 					res.sendStatus(400);
+// 				} else {
+// 					res.sendStatus(202);
+// 				}
+// 			});
+// 		}
+// 	});
+
+
+// });
+
+// app.get('/app', function(req, res) {
+// 	res.sendFile(__dirname + '/public/index.html');
+// });
 
 
 app.listen(3000, function() {
