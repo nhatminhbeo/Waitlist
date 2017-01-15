a075e0ca09f7c9ecc26212fbd4dc9d0500bb5009
Justin Huynh <juh079@ucsd.edu>
2017-01-14 16:51:20 -0800

    wow

--- public/edit.js
+++ public/edit.js
@@ -1,15 +1,16 @@
-$document.ready(function(){
+$(document).ready(function(){
 	var currentLocation = window.location.href;
 
 	//returns an array of all the string's parts
 	var splitstring = currentLocation.split('/');
 	//the id should be in this last array index 
-	var id = splitstring[splitstring.length() - 1];
+	var id = splitstring[splitstring.length - 1];
 
 	$.get("/user/" + id , function(data){
-		$('#name').append(data["name"]);
-		$('#phone').append(data["phone"]);
-		$('#nop').append(data["guest"]);
+		//console.log(data);
+		$('#name').val(data["name"]);
+		$('#phone').val(data["phone"]);
+		$('#nop').val(data["guests"]);
 	});	
 
 	$("form").submit(function(event) {
@@ -18,22 +19,38 @@ $document.ready(function(){
 
 		var currentLocation = window.location.href;
 		var splitstring = currentLocation.split('/');
-		var id = splitstring[splitstring.length() - 1];
+		var id = splitstring[splitstring.length - 1];
 
 		// Gather data
 		var name = $("#name").val();
 		var guests = $("#nop").val();
 		var phone = $("#phoneNumber").val();
 
-		var data = {
-			name: name,
-			guests: guests,
-			phone: phone
-		};
+		
 
-		//Fire off that data
-		$.put("/user/" + id, data, function() {
-			console.log("posted!");
+		$.get("/user/" + id , function(data){
+			var rev = data["_rev"];
+			var preferences = data["preferences"];
+			var data = {
+				name: name,
+				guests: guests,
+				phone: phone ,
+				guests: guests,
+				preferences: preferences,
+				_id : id, 
+				_rev : rev
+			};
+
+			//Fire off that data
+			$.ajax({
+				url:"/user",
+				data:data,
+				type: 'PUT',
+				success: function(response) {
+					console.log("posted!");
+				}
+			});
 		});
 	});
 });
+
--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	// var currentLocation = window.location.href;
-	// var splitstring = currentLocation.split('/');
-	// var id = splitstring[splitstring.length() - 1];
-	// console.log(id);
-	getPosts(10);
+	var currentLocation = window.location.href;
+	var splitstring = currentLocation.split('/');
+	var id = splitstring[splitstring.length - 1];
+	console.log(id);
+	getPosts(id);
 	//handle the buton click()
+
+
 });
 
 function getPosts(id) {
@@ -13,11 +15,14 @@ function getPosts(id) {
 	$("#content").text("");
 
 	$.get('/waitlist/', function(data) {
+		if(id=="restaurant"){
+			$('#content').append('<form action="http://localhost:3000/app/restaurant"><button class="delete">delete</button></form>')
+		}
 	// console.log(data);
 		for (var i=0; i<data.length; i++) {
 			// Find the important stuff inside data
 			var name = data[i]["name"];
-			var numberOfPeople = data[i]["guest"];
+			var numberOfPeople = data[i]["guests"];
 			var phoneNumber = data[i]['phone'];
 
 			// Generate the html we want to insert
@@ -26,11 +31,21 @@ function getPosts(id) {
 				'number of people:<p class="card--content">' + numberOfPeople + '</p></div>';
 			// Insert the card HTML
 			if(data[i]["_id"] == id){
-				$('#content').append(card + '<button id="edit--post" href="/edit/{{id}}">edit</button>');
+				$('#content').append(card + '<button id="edit--post" onclick="location.href=\'/edit/' + id + '\';">edit</button>');
 			}
 			else{
 				$('#content').append(card);
 			}
-		}		
+		}
+		$('.delete').click(function() {
+				//Fire off that data
+			$.ajax({
+				url:"/waitlist/",
+				type: 'DELETE',
+				success: function(response) {
+					console.log("deleted!");
+				}
+			});
+});		
 	});
 }
